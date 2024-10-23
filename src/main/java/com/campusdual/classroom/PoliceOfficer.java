9c67625c5491661493754241b5f8156afcfdca2c
Lorena Sambad Lema <lsambadlema@gmail.com>
2024-10-23 17:03:39 +0200

    Terminado Exercise20

--- src/main/java/com/campusdual/classroom/PoliceOfficer.java
+++ src/main/java/com/campusdual/classroom/PoliceOfficer.java
@@ -8,4 +8,10 @@ public class PoliceOfficer extends Person {
 		super(name, surname);
 		this.squad = squad;
 	}
+
+
+	@Override
+	public void getDetails(){
+		System.out.println("Oficial de policia " +name + " " + surname + ", escuadrón " + squad);
+	}
 }
